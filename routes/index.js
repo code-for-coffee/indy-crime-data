a9492b7376a0a0de0c128ad0c5efdae52bd173fa
Aaron <akr.git.receiver@gmail.com>
2016-06-04 14:56:38 -0400

    site colors done, added nav to layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var indexInfo = {
+  title: 'Indy Crime Data',
+
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', indexInfo);
 });
 
 module.exports = router;
