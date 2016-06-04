3570407bf59aa18a1f3946bc99cc877fb15231ed
Aaron <akr.git.receiver@gmail.com>
2016-06-04 15:18:43 -0400

    added map styling and other pages

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var indexInfo = {
   title: 'Indy Crime Data',
+  desc: 'One stop shop to view commited crime locations in Indianapolis.'
 
 }
 
@@ -10,5 +11,13 @@ var indexInfo = {
 router.get('/', function(req, res, next) {
   res.render('index', indexInfo);
 });
+
+router.get('/map', function(req, res, next){
+  res.render('map');
+})
+
+router.get('/about', function(req, res, next){
+  res.render('about', indexInfo)
+})
 
 module.exports = router;
