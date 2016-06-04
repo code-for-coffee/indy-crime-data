33ab9b7b72cee1798d28ac7d3bca304331bc984b
X Coan <xcoan@purdue.edu>
2016-06-04 17:47:12 -0400

    markers!

--- public/javascripts/map.server.js
+++ public/javascripts/map.server.js
@@ -50,20 +50,51 @@ $(document).ready(function(){
     }
   });
 });
+
+var mapcoords= {
+  "loc1" : [{
+    "latitude": 39.80936,
+    "longitude": -86.15047,
+    "crime": "AGGRIVATED ASSAULT-GUN"
+  }, {
+    "latitude": 39.77001,
+    "longitude": -86.26499,
+    "crime": "AGGRIVATED ASSAULT-GUN"
+  }, {
+    "latitude": 39.77446,
+    "longitude": -86.12026,
+    "crime": "AGGRIVATED ASSAULT-GUN"
+  }, {
+    "latitude": 39.77235,
+    "longitude": -86.13031,
+    "crime": "AGGRIVATED ASSAULT-GUN"
+  }, {
+    "latitude": 39.74383,
+    "longitude": -86.14706,
+    "crime": "AGGRIVATED ASSAULT-GUN"
+  }]
+}
+
 // console.log(data.features[1].properties);
 //
 // function addMarker(data){
 //   var marker = new google.maps.Marker({
 //     position: {lat: data.features.properties.X_COORD}
 //   })
-// function addMarker (data){
-//   var marker = new google.maps.Marker({
-//   position: {lat:parseFloat(data.latitude),lng:parseFloat(data.longitude)},
-//   map: map,
-//   icon:'http://megaicons.net/static/img/icons_sizes/15/534/32/map-marker-ball-left-pink-icon.png',
-//   title: data.primary_type+' ---- '+data.description+' ---- '+data.date,
-//   animation:google.maps.Animation.DROP
-// });// end of marker constructor
-//     markers.push(marker);
-//   }
-// }
+function addMarker (data){
+  var marker = new google.maps.Marker({
+  position: data,
+  map: map,
+  icon:'http://megaicons.net/static/img/icons_sizes/15/534/32/map-marker-ball-left-pink-icon.png',
+  title: data.primary_type+' ---- '+data.description+' ---- '+data.date,
+  animation:google.maps.Animation.DROP
+});// end of marker constructor
+    markers.push(marker);
+  }
+  for (int i = 0; i < mapcoords.length; i++) {
+    var lati = mapcoords.loc1[i].latitude;
+    var long = mapcoords.loc1[i].longitude;
+    crime = {lat: lati, lng: long};
+    addMarker(crime);
+  }
+  //}
