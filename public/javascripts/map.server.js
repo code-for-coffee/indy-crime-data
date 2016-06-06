5c7b3fa78ee414a96abb3c39ae055d754f3705e1
Aaron <akr.git.receiver@gmail.com>
2016-06-06 16:19:27 -0400

    updated some style + added more map functionality + commnted out looping code 4 map

--- public/javascripts/map.server.js
+++ public/javascripts/map.server.js
@@ -58,11 +58,37 @@ $(document).ready(function(){
     title: 'we did it again',
     desc: 'some more data here'
   }
+  var avar = "I'M PART OF THE FIRST HEADING";
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">'+avar+'</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
   var map = new google.maps.Map(document.getElementById('googleMap'), examplePoint, console.log('hit in map var'));
-
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
   var marker = new google.maps.Marker({
     position: myLatLng,
     title: titleData.title + titleData.desc
+  })
+  marker.addListener('click', function(){
+    infowindow.open(map, marker);
   })
 
   marker.setMap(map);
@@ -71,12 +97,24 @@ $(document).ready(function(){
     type:'get',
     success:function(data){
       console.log(data);
+      let indy = " INDIANAPOLIS, IN";
+      indy.toUpperCase();
       var tumble = data.features[1].properties.ADDRESS+' Indianapolis, IN'
       console.log(tumble);
-      for (var i = 0; i < data.features.length; i ++){
-        console.log(data.features[i]);
-        console.log('hit em: ' + i);
-      }
+      //~~~~~~~~~~~~~~~~~~~~~~~~~
+      //~~~~~~~~GOOD CODE~~~~~~~~
+      //=======DONT DELETE=======
+      //~~~~~~~~GOOD CODE~~~~~~~~
+      //~~~~~~~~~~~~~~~~~~~~~~~~~
+          // for (var i = 0; i < data.features.length; i ++){
+          //   console.log(data.features[i].properties.ADDRESS + indy);
+          //   console.log('hit em: ' + i);
+          // }
+      //~~~~~~~~~~~~~~~~~~~~~~~~~
+      //~~~~~~~~GOOD CODE~~~~~~~~
+      //=======DONT DELETE=======
+      //~~~~~~~~GOOD CODE~~~~~~~~
+      //~~~~~~~~~~~~~~~~~~~~~~~~~
       //for(var crime in data){
       //  if(data[crime].primary_type==$('option:selected').val()){
       //    addMarker(data[crime]);
