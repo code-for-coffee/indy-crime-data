eba2674d069ea9d7c202c9bd39bd6b642c50adbf
Aaron <akr.git.receiver@gmail.com>
2016-06-09 10:49:57 -0400

    refactored map infowindow for dynamicness

--- public/javascripts/map.server.js
+++ public/javascripts/map.server.js
@@ -18,39 +18,16 @@ $(document).ready(function(){
   }
   var avar = "I'M PART OF THE FIRST HEADING";
   var map = new google.maps.Map(document.getElementById('googleMap'), examplePoint, console.log('hit in map var'));
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">'+avar+'</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>BINGBONG</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
   var geocoder = new google.maps.Geocoder();
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    title: titleData.title + titleData.desc
-  })
-  marker.addListener('click', function(){
-    infowindow.open(map, marker);
-  })
+  // var marker = new google.maps.Marker({
+  //   position: myLatLng,
+  //   title: titleData.title + titleData.desc
+  // })
+  // marker.addListener('click', function(){
+  //   infowindow.open(map, marker);
+  // })
 
-  marker.setMap(map);
+  //marker.setMap(map);
   $.ajax({
     url:'http://data.indy.gov/datasets/5c8a9412cf454753bc774d31d8505b50_12.geojson',
     type:'get',
@@ -119,6 +96,29 @@ $(document).ready(function(){
       lat: aLat,
       lng: aLng
     }
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">'+avar+'</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>BINGBONG</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
       var marker = new google.maps.Marker({
       position: objLatLng,
       map: map,
