7604c637dedcf41bcb5ce2ccf08240f4c6cf16b3
Aaron <akr.git.receiver@gmail.com>
2016-06-09 23:23:28 -0400

    pushing up buggy code to review

--- public/javascripts/map.server.js
+++ public/javascripts/map.server.js
@@ -32,39 +32,57 @@ $(document).ready(function(){
     url:'http://data.indy.gov/datasets/5c8a9412cf454753bc774d31d8505b50_12.geojson',
     type:'get',
     success:function(data){
-      console.log(data);
       let indy = " INDIANAPOLIS, IN";
       //~~~~~~~~~~~~~~~~~~~~~~~~~
       //~~~~~~~~GOOD CODE~~~~~~~~
       //=======DONT DELETE=======
       //~~~~~~~~GOOD CODE~~~~~~~~
       //~~~~~~~~~~~~~~~~~~~~~~~~~
-      for (var i = 0; i < 11; i ++){
+      var putArr = [];
+      for (var i = 5000; i < 5009; i ++){
         // console.log(data.features[i].properties.ADDRESS + indy);
         // console.log('hit em: ' + i);
+        // var props = data.features[i].properties;
+        // var crimeString = data.features[i].properties.CRIME;
+        // var markerColor = crimeType(crimeString);
+        //console.log(data.features[i]);
         var crimeString = data.features[i].properties.CRIME;
         var markerColor = crimeType(crimeString);
+        putArr.push(data.features[i].properties);
+        // console.log(putArr);
         geocoder.geocode({'address': data.features[i].properties.ADDRESS + indy}, function(results, status){
           if(status === google.maps.GeocoderStatus.OK){
-            console.log(data + "::::" + results);
-              addMarker(data, results, markerColor, crimeString);
+            addMarker(putArr, results, markerColor, crimeString);
           }else{
             console.log(status + ':::' + results);
           }
         })
       }
-      //~~~~~~~~~~~~~~~~~~~~~~~~~
-      //~~~~~~~~GOOD CODE~~~~~~~~
-      //=======end of good=======
-      //~~~~~~~~GOOD CODE~~~~~~~~
-      //~~~~~~~~~~~~~~~~~~~~~~~~~
     },
     error:function(err){
       console.log(err);
     }
   });
 
-
+  // var contentString = '<div id="content">'+
+  //     '<div id="siteNotice">'+
+  //     '</div>'+
+  //     '<h1 id="firstHeading" class="firstHeading">SAD</h1>'+
+  //     '<div id="bodyContent">'+
+  //     '<p><b>'+props.ADDRESS+'+</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  //     'sandstone rock formation in the southern part of the '+
+  //     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+  //     'south west of the nearest large town, Alice Springs; 450&#160;km '+
+  //     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+  //     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+  //     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+  //     'Aboriginal people of the area. It has many springs, waterholes, '+
+  //     'rock caves and ancient paintings. Uluru is listed as a World '+
+  //     'Heritage Site.</p>'+
+  //     '<p>'+
+  //     ': the address.</p>'+
+  //     '</div>'+
+  //     '</div>';
   //More crimes need to be added
   function crimeType(crimeString) {
       if (crimeString.includes("ROBBERY")) {
@@ -88,46 +106,96 @@ $(document).ready(function(){
       mapTypeId:google.maps.MapTypeId.ROADMAP
     };
   }
-  function addMarker(data, LatLng, markerColorl, crimeString){
+  // function createInfoWindow(marker, strContent){
+  //   google.maps.event.addListener(marker, 'click', function () {
+  //           infoWindow.setContent(strContent);
+  //           infoWindow.open(map, this);
+  //       });
+  // }
+  // function createInfoWindow(marker, popupContent) {
+  //     var infowindow = new google.maps.InfoWindow({
+  //       content: contentString
+  //     });
+  //    google.maps.event.addListener(marker, 'click', function () {
+  //        infoWindow.setContent(contContent);
+  //        infoWindow.open(map, this);
+  //    });
+  //  }
+  function addMarker(data, LatLng, markerColor, crimeString, props){
     //console.log(LatLng[0].geometry.location.lat()+''+LatLng[0].geometry.location.lng());
     var aLat = LatLng[0].geometry.location.lat();
     var aLng = LatLng[0].geometry.location.lng();
+    // var objInfo = {
+    //   crime: data.properties.ADDRESS
+    // }
+    // for(i=0; i < data.length; i++){
+    //   var contentString = '<div id="content">'+
+    //       '<div id="siteNotice">'+
+    //       '</div>'+
+    //       '<h1 id="firstHeading" class="firstHeading">SAD</h1>'+
+    //       '<div id="bodyContent">'+
+    //       '<p><b>'+data[i].ADDRESS+'+</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+    //       'sandstone rock formation in the southern part of the '+
+    //       'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+    //       'south west of the nearest large town, Alice Springs; 450&#160;km '+
+    //       '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+    //       'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+    //       'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+    //       'Aboriginal people of the area. It has many springs, waterholes, '+
+    //       'rock caves and ancient paintings. Uluru is listed as a World '+
+    //       'Heritage Site.</p>'+
+    //       '<p>'+
+    //       ': the address.</p>'+
+    //       '</div>'+
+    //       '</div>';
+    //   //creating infowindow
+    //   var infowindow = new google.maps.InfoWindow({
+    //     content: contentString
+    //   });
+    //   marker.addListener('click', function(){
+    //     infowindow.open(map, marker);
+    //   })
+    // }
     var objLatLng = {
       lat: aLat,
       lng: aLng
     }
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">'+avar+'</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>BINGBONG</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-      var marker = new google.maps.Marker({
-      position: objLatLng,
-      map: map,
-      icon: 'http://maps.google.com/mapfiles/ms/icons/' + markerColor,
-      title: crimeString,
-      animation: google.maps.Animation.DROP
-    });
-    marker.addListener('click', function(){
-      infowindow.open(map, marker);
-    })
+    for(i=0; i < data.length; i++){
+      var redata = data[i];
+      console.log(redata);
+      var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">'+objLatLng.lat+' '+objLatLng.lng+'</h1>'+
+          '<div id="bodyContent">'+
+          '<p><b>'+redata.ADDRESS+'+</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+          'sandstone rock formation in the southern part of the '+
+          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+          'south west of the nearest large town, Alice Springs; 450&#160;km '+
+          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+          'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+          'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+          'Aboriginal people of the area. It has many springs, waterholes, '+
+          'rock caves and ancient paintings. Uluru is listed as a World '+
+          'Heritage Site.</p>'+
+          '<p>'+
+          ': the address.</p>'+
+          '</div>'+
+          '</div>';
+          var marker = new google.maps.Marker({
+            position: objLatLng,
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/' + markerColor,
+            title: crimeString,
+            animation: google.maps.Animation.DROP
+          }).addListener('click', function(){
+            infowindow.open(map, this);
+            console.log(infowindow);
+          });;
+          var infowindow = new google.maps.InfoWindow({
+                content: contentString
+          });
+
+    }
   }
 });
