8ae7ae8fc1813c65a6255549fea073b9989c8e83
Aaron <akr.git.receiver@gmail.com>
2016-06-12 14:00:27 -0400

    added font awesome, style changes, and semi-working user register + login

--- routes/users.js
+++ routes/users.js
@@ -8,45 +8,55 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-router.get('/', function(req, res){
+router.get('/user', function(req, res){
+  console.log('got' + user);
   res.render('user', {
     user:req.user
   })
 });
 
-router.get('/login', function(req, res){
+router.get('/user/login', function(req, res){
   res.render('login', {
     user:req.user
   })
 });
 //on failrue redirect home
-router.post('/login', passport.authenticate('local', {failureRedirect: '/'}),
+router.post('/user/login', passport.authenticate('local', {failureRedirect: '/'}),
   function(req, res){
     res.redirect('/');
   }
 );
 
-router.get('/register', function(req, res){
-  res.render('register', {
-    user:req.user
-  })
-});
+// router.get('/register', function(req, res){
+//   User.register(new User({
+//     username: req.body.username,
+//     email: req.body.email
+//   }),
+//   req.body.password,
+//   )
+//   res.render('/', {
+//     user:req.user
+//   })
+// });
 
-router.get('/logout', function(req,res){
+router.get('/user/logout', function(req,res){
   req.logout();
   res.redirect('/');
 });
 
-router.post('/register', function(req, res){
+router.post('/user/register', function(req, res){
   User.register(new User({
     username: req.body.username
   }),
   req.body.password,
   function(err, account){
+    console.log('in error or create function');
     if(err){
-      return res.render('register', { user: user});
+      console.log('we hit error and didn\'t authenticate');
+      return res.render('/index', err);
     }
     passport.authenticate('local')(req, res, function(){
+      console.log('passport authenticated redirecting');
       res.redirect('/');
     });
   });
